0c81e1a7496c149f3f7be5bfd912cf1f9a0d062d
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-28 18:16:36 +0200

    chore: update extra files\n> Unsure what they reflect atm, but have had changes during work on the system

---
+++ Assets/Scripts/GameMechanics/GunBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehaviour : MonoBehaviour
+{
+
+    private PlayerAction playerAction;    
+    public GameObject holder;
+    public GameObject gun;
+    public GameObject bulletPrefab;
+    private Vector3 bulletOffset = new Vector3(0f, 0f, 1f);
+
+    void Start()
+    {
+        playerAction = FindObjectOfType<PlayerAction>();
+        playerAction.shoot += OnHolderShoot;
+
+        holder = GameObject.Find("Player");
+        this.transform.SetParent(holder.transform);
+
+        gun = GameObject.Find("Gun");
+    }
+
+    void OnHolderShoot()
+    {
+        SpawnBullet();        
+    }
+
+    void SpawnBullet()
+    {
+        Vector3 spawnPosition = bulletOffset;
+        Quaternion spawnRotation = transform.rotation;
+
+        GameObject bullet = Instantiate(bulletPrefab, spawnPosition, spawnRotation);
+    }
+
+    void OnDestroy()
+    {
+        playerAction.shoot -= OnHolderShoot;    
+    }
+}
---
+++ Assets/Scripts/GameMechanics/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 5f;
+    public float jumpForceMagnitude = 5f;
+    public float mouseSensitivity = 5f;
+
+    private Rigidbody playerRigidBody;
+
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+
+        // player won't rotate along any horizontal axis
+        playerRigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        this.handlePlayerRotation();
+        this.handlePlayerJump();
+    }
+
+    void FixedUpdate()
+    {
+        this.handlePlayerMovement();
+    }
+
+    private void handlePlayerMovement()
+    {
+        Vector3 movementDirectionVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            movementDirectionVector += this.transform.forward;
+
+        if (Input.GetKey(KeyCode.A))
+            movementDirectionVector += -this.transform.right;
+
+        if (Input.GetKey(KeyCode.S))
+            movementDirectionVector += -this.transform.forward;
+
+        if (Input.GetKey(KeyCode.D))
+            movementDirectionVector += this.transform.right;
+
+        movementDirectionVector = movementDirectionVector.normalized * Time.fixedDeltaTime * movementSpeed;
+        playerRigidBody.MovePosition(playerRigidBody.position + movementDirectionVector);
+    }
+
+    private void handlePlayerJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && this.isAtTheGround())
+        {
+            playerRigidBody.AddForce(Vector3.up * jumpForceMagnitude, ForceMode.VelocityChange);
+        }
+    }
+
+    private void handlePlayerRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(mouseSensitivity * mouseX * Vector3.up); // rotation through Y axis
+    }
+
+    private bool isAtTheGround()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f);
+    }
+}
