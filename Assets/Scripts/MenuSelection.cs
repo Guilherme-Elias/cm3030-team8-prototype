7ca450ceb9aabdbcd250494f121f85b01a83b833
Guilherme-Elias <guilhermemarinho524@gmail.com>
2025-08-06 22:26:29 -0400

    feat(Scenes) : adds a new scene for the main menu.

---
+++ Assets/Scripts/MenuSelection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSelection : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("A new game will start!");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Settings pannel will be opened.");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Seeya!");
+    }
+}
