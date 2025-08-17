8a79499abe84f90a0949472d87a2e0ace0729aa4
Guilherme-Elias <guilhermemarinho524@gmail.com>
2025-08-17 18:23:37 -0400

    feat(Scenes\Game) : adds player health logic.

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerAction : MonoBehaviour
 {
     private GunController gunController;
+    private const float MAX_HEALTH = 100;
+    private float currentHealth = 0;
 
     void Start()
     {
+        RestoreHealth(MAX_HEALTH);
         gunController = FindObjectOfType<GunController>(); // in GunManager 
     }
 
@@ -32,5 +36,34 @@ public class PlayerAction : MonoBehaviour
     private void Reload()
     {
         gunController.Reload();
+    }
+
+    private void RestoreHealth(float amount)
+    {
+        float futureHealth = this.currentHealth + amount;
+        if (futureHealth >= MAX_HEALTH)
+        {
+            this.currentHealth = MAX_HEALTH;
+            return;
+        }            
+        this.currentHealth = futureHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        float futureHealth = this.currentHealth - amount;
+        if (futureHealth <= 0)
+        {
+            Die();
+        }
+
+        this.currentHealth = futureHealth;
+    }
+
+    public void Die()
+    {
+        // todo: endgame logic
+        Debug.Log("You have died!");
+        
     }
 }
